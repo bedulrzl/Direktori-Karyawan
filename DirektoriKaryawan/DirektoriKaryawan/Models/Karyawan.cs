76a0544f5c67eae82264dcfe3089122b0dfbeb73
aprizalrzl@outlook.com <aprizalrzl@outlook.com>
2019-02-21 11:34:15 +0700

    add model karyawan

---
+++ DirektoriKaryawan/DirektoriKaryawan/Models/Karyawan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DirektoriKaryawan.Models
+{
+    public class Karyawan
+    {
+        public int karyawanID { get; set; }
+        public string Nama { get; set; }
+        public string Jabatan { get; set; }
+    }
+}
