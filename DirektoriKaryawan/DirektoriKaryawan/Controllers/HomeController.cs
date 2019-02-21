bbbe641c7b452893511486c78a986beca6f5c5d8
bedulrzl <aprizalbedul@gmail.com>
2019-02-21 11:17:48 +0700

    Update HomeController.cs

--- DirektoriKaryawan/DirektoriKaryawan/Controllers/HomeController.cs
+++ DirektoriKaryawan/DirektoriKaryawan/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using DirektoriKaryawan.Models;
-
+//test
 namespace DirektoriKaryawan.Controllers
 {
     public class HomeController : Controller
